bf38d4d393dca2a498634ecb0586b6b446b2c6c1
Leendert de Borst <ldeborst@xivisoft.com>
2024-06-08 01:28:14 +0200

    Update entrypoint.sh

--- src/AliasVault.WebApp/entrypoint.sh
+++ src/AliasVault.WebApp/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# Set the default API URL for localhost debugging
+DEFAULT_API_URL="http://localhost:81"
 
-# Replace placeholder with the actual API URL
-sed -i "s|http://localhost:5092|${API_URL}|g" /app/wwwroot/appsettings.json
+# Use the provided API_URL environment variable if it exists, otherwise use the default
+API_URL=${API_URL:-$DEFAULT_API_URL}
+
+# Replace the default URL with the actual API URL
+sed -i "s|http://localhost:5092|${API_URL}|g" /usr/share/nginx/html/appsettings.json
 
 # Start the application
 nginx -g "daemon off;"
