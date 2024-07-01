53ea7c24775fd3acb8b06dbb8c74e2d04f1838ea
Leendert de Borst <ldeborst@xivisoft.com>
2024-07-01 12:46:50 +0200

    Remove client tables from server db (#58)

--- src/Databases/AliasServerDb/AliasServerDbContext.cs
+++ src/Databases/AliasServerDb/AliasServerDbContext.cs
@@ -33,26 +33,6 @@ public class AliasServerDbContext : IdentityDbContext<AliasVaultUser>
     }
 
     /// <summary>
-    /// Gets or sets the Identities DbSet.
-    /// </summary>
-    public DbSet<Identity> Identities { get; set; }
-
-    /// <summary>
-    /// Gets or sets the Logins DbSet.
-    /// </summary>
-    public DbSet<Login> Logins { get; set; }
-
-    /// <summary>
-    /// Gets or sets the Passwords DbSet.
-    /// </summary>
-    public DbSet<Password> Passwords { get; set; }
-
-    /// <summary>
-    /// Gets or sets the Services DbSet.
-    /// </summary>
-    public DbSet<Service> Services { get; set; }
-
-    /// <summary>
     /// Gets or sets the AspNetUserRefreshTokens DbSet.
     /// </summary>
     public DbSet<AspNetUserRefreshToken> AspNetUserRefreshTokens { get; set; }
@@ -88,41 +68,6 @@ public class AliasServerDbContext : IdentityDbContext<AliasVaultUser>
                 }
             }
         }
-
-        // Configure Identity - Login relationship
-        builder.Entity<Login>()
-            .HasOne(l => l.Identity)
-            .WithMany()
-            .HasForeignKey(l => l.IdentityId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        // Configure the Login - UserId entity
-        builder.Entity<Login>()
-            .HasOne(p => p.User)
-            .WithMany()
-            .HasForeignKey(p => p.UserId)
-            .IsRequired();
-
-        // Configure Login - Service relationship
-        builder.Entity<Login>()
-            .HasOne(l => l.Service)
-            .WithMany()
-            .HasForeignKey(l => l.ServiceId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        // Configure Login - Password relationship
-        builder.Entity<Login>()
-            .HasMany(l => l.Passwords)
-            .WithOne(p => p.Login)
-            .HasForeignKey(p => p.LoginId)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        // Configure Identity - DefaultPassword relationship
-        builder.Entity<Identity>()
-            .HasOne(i => i.DefaultPassword)
-            .WithMany()
-            .HasForeignKey(i => i.DefaultPasswordId)
-            .OnDelete(DeleteBehavior.SetNull);
 
         // Configure the User - AspNetUserRefreshToken entity
         builder.Entity<AspNetUserRefreshToken>()
