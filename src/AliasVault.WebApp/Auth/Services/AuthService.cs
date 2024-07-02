b8ffd39f999e28b769d7e7cb70e3010b59318e87
Leendert de Borst <ldeborst@xivisoft.com>
2024-07-02 23:15:38 +0200

    Revert encryption key debug statement (#81)

--- src/AliasVault.WebApp/Auth/Services/AuthService.cs
+++ src/AliasVault.WebApp/Auth/Services/AuthService.cs
@@ -108,9 +108,8 @@ public class AuthService(HttpClient httpClient, ILocalStorageService localStorag
     public string GetEncryptionKeyAsBase64Async()
     {
         // Enable this line for debugging to skip unlock screen.
-        return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB=";
-
-        // return Convert.ToBase64String(GetEncryptionKeyAsync());
+        // return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB=";
+        return Convert.ToBase64String(GetEncryptionKeyAsync());
     }
 
     /// <summary>
