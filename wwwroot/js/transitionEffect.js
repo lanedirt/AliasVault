50c3c64db6f52df02bf129ad1eec16f2dd5a6d1d
Leendert de Borst <ldeborst@xivisoft.com>
2024-10-28 13:36:36 +0100

    Delete transitionEffect.js (#177)

--- wwwroot/js/transitionEffect.js
+++
@@ -1,8 +0,0 @@
-function startTransitionEffect() {
-    const overlay = document.getElementById('transitionOverlay');
-    overlay.style.transform = 'scale(1)';
-
-    setTimeout(() => {
-        window.location.href = '/user/register';
-    }, 500);
-}
