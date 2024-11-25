af9fba39f33b77379fc6262ada245ea38777ba33
Leendert de Borst <ldeborst@xivisoft.com>
2024-11-25 15:30:59 +0100

    Update AppInfo.cs (#415)

--- src/Shared/AliasVault.Shared.Core/AppInfo.cs
+++ src/Shared/AliasVault.Shared.Core/AppInfo.cs
@@ -25,12 +25,12 @@ public static class AppInfo
     /// <summary>
     /// Gets the minor version number.
     /// </summary>
-    public const int VersionMinor = 7;
+    public const int VersionMinor = 8;
 
     /// <summary>
     /// Gets the patch version number.
     /// </summary>
-    public const int VersionPatch = 0;
+    public const int VersionPatch = 1;
 
     /// <summary>
     /// Gets the build number, typically used in CI/CD pipelines.
