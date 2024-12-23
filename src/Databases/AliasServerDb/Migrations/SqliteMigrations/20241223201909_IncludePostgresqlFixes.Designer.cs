6a0699318c8a6b2ddd76031f33c151809a4719b6
Leendert de Borst <ldeborst@xivisoft.com>
2024-12-23 21:41:25 +0100

    Add sqlite migrations to be in sync with postgresql model (#190)

--- src/Databases/AliasServerDb/Migrations/SqliteMigrations/20241223201909_IncludePostgresqlFixes.Designer.cs
+++ src/Databases/AliasServerDb/Migrations/SqliteMigrations/20241223201909_IncludePostgresqlFixes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AliasServerDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AliasServerDb.Migrations.SqliteMigrations
 {
     [DbContext(typeof(AliasServerDbContextSqlite))]
-    partial class AliasServerDbContextSqliteModelSnapshot : ModelSnapshot
+    [Migration("20241223201909_IncludePostgresqlFixes")]
+    partial class IncludePostgresqlFixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
