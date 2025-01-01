8f8b4af3c987fc5f5e278a3acd04288b98ecfbb6
Leendert de Borst <ldeborst@xivisoft.com>
2025-01-01 16:18:15 +0100

    Update install.sh (#510)

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @version 0.10.0
+# @version 0.10.1
 
 # Repository information used for downloading files and images from GitHub
 REPO_OWNER="lanedirt"
--- src/Shared/AliasVault.Shared.Core/AppInfo.cs
+++ src/Shared/AliasVault.Shared.Core/AppInfo.cs
@@ -30,7 +30,7 @@ public static class AppInfo
     /// <summary>
     /// Gets the patch version number.
     /// </summary>
-    public const int VersionPatch = 0;
+    public const int VersionPatch = 1;
 
     /// <summary>
     /// Gets the build number, typically used in CI/CD pipelines.
