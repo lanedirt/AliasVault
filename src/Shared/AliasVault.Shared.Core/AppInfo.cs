d7f1df32524c199e707edd2525768eddf7d02db3
Leendert de Borst <ldeborst@xivisoft.com>
2025-01-01 14:56:28 +0100

    Update AppInfo bump version (#508)

--- src/Shared/AliasVault.Shared.Core/AppInfo.cs
+++ src/Shared/AliasVault.Shared.Core/AppInfo.cs
@@ -25,12 +25,12 @@ public static class AppInfo
     /// <summary>
     /// Gets the minor version number.
     /// </summary>
-    public const int VersionMinor = 9;
+    public const int VersionMinor = 10;
 
     /// <summary>
     /// Gets the patch version number.
     /// </summary>
-    public const int VersionPatch = 4;
+    public const int VersionPatch = 0;
 
     /// <summary>
     /// Gets the build number, typically used in CI/CD pipelines.
