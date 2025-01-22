d23f8793b251bf9e3648fe0dd22ada3400260a7c
Leendert de Borst <ldeborst@xivisoft.com>
2025-01-22 15:37:15 +0100

    Add sample button component (#541)

--- browser-extensions/chrome/src/components/App.tsx
+++ browser-extensions/chrome/src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/tailwind.css';
+import Button from './Button';
 
 const App: React.FC = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <div className="bg-blue-500 text-white p-4">
       <h1>Hello, AliasVault Chrome Extension!</h1>
+      <div className="mt-4">
+        <Button onClick={handleClick}>
+          Click me!
+        </Button>
+      </div>
     </div>
   );
 };
