267cd6e9f6c6439c02fc803ea3d051dc2e318d61
Leendert de Borst <ldeborst@xivisoft.com>
2025-02-05 13:44:00 +0100

    Refactor vite config to fix import conflicts (#541)

--- browser-extensions/chrome/vite.config.js
+++ browser-extensions/chrome/vite.config.js
@@ -1,76 +1,52 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import webExtension from 'vite-plugin-web-extension';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import dictionaryLoader from './vite-plugins/dictionary-loader';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
     dictionaryLoader(),
-    viteStaticCopy({
-        targets: [
-        {
-            src: 'src/manifest.json',
-            dest: '.' // Copy to the root of the dist folder
-        },
-        {
-            src: 'assets/*',
-            dest: 'assets'
-        },
-        // Copy Argon2 WASM file loaded separately by the Argon2 library.
-        {
-            src: 'node_modules/argon2-browser/dist/argon2.wasm',
-            dest: 'src'
-        },
-        // Copy SQL.js WASM file loaded separately by the SqliteClient.
-        {
-            src: 'node_modules/sql.js/dist/sql-wasm.wasm',
-            dest: 'src'
-        },
-        {
-          src: 'src/styles/contentScript.css',
-          dest: '.'
-        }
-        ]
+    webExtension({
+      manifest: 'src/manifest.json',
     }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/*',
+          dest: 'assets'
+        },
+        {
+          src: 'node_modules/argon2-browser/dist/argon2.wasm',
+          dest: 'src'
+        },
+        {
+          src: 'node_modules/sql.js/dist/sql-wasm.wasm',
+          dest: 'src/src'
+        }
+      ]
+    })
   ],
   css: {
     postcss: {
       plugins: [
         tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        app: './index.html',
-        background: './src/background.ts',
-        contentScript: './src/contentScript.ts',
-        contentStyles: './src/styles/contentScript.css'
-      },
-      output: {
-        entryFileNames: '[name].js',
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith('contentScript.css')) {
-            return 'contentScript.css';
-          }
-          return '[name].[ext]';
-        },
-        format: 'es'
-      }
-    },
-    outDir: 'dist',
+        autoprefixer
+      ]
+    }
   },
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': '/src'
+    }
   },
   server: {
     open: '/src/popup.html'
   },
+  build: {
+    outDir: 'dist'
+  }
 });
