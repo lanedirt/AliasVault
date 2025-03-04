613fb7db1273005d646aefa775a6154d641821e0
Leendert de Borst <ldeborst@xivisoft.com>
2025-03-04 17:42:06 +0100

    Refactor browser extension info to shared constants (#628)

---
+++ src/Shared/AliasVault.Shared.Core/BrowserExtensions/BrowserType.cs
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------------
+// <copyright file="BrowserType.cs" company="lanedirt">
+// Copyright (c) lanedirt. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AliasVault.Shared.Core.BrowserExtensions;
+
+/// <summary>
+/// The type of browser.
+/// </summary>
+public enum BrowserType
+{
+    /// <summary>
+    /// Unknown browser type.
+    /// </summary>
+    Unknown,
+
+    /// <summary>
+    /// Firefox browser type.
+    /// </summary>
+    Firefox,
+
+    /// <summary>
+    /// Chrome browser type.
+    /// </summary>
+    Chrome,
+
+    /// <summary>
+    /// Safari browser type.
+    /// </summary>
+    Safari,
+
+    /// <summary>
+    /// Edge browser type.
+    /// </summary>
+    Edge,
+
+    /// <summary>
+    /// Brave browser type.
+    /// </summary>
+    Brave,
+}
