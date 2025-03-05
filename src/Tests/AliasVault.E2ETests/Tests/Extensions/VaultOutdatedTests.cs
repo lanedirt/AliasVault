9e478c94f9f214be0edabad5acbeb248e74aeeea
Leendert de Borst <ldeborst@xivisoft.com>
2025-03-05 17:17:43 +0100

    Add browser extension vault outdate check, fix db create wait flow (#643)

---
+++ src/Tests/AliasVault.E2ETests/Tests/Extensions/VaultOutdatedTests.cs
@@ -0,0 +1,48 @@
+//-----------------------------------------------------------------------
+// <copyright file="VaultOutdatedTests.cs" company="lanedirt">
+// Copyright (c) lanedirt. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AliasVault.E2ETests.Tests.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// End-to-end tests for the Chrome extension involving simulating an outdated vault
+/// to check for expected error messages.
+/// </summary>
+[Parallelizable(ParallelScope.Self)]
+[Category("ExtensionTests")]
+[TestFixture]
+public class VaultOutdatedTests : BrowserExtensionPlaywrightTest
+{
+    /// <summary>
+    /// Tests if the extension correctly handles an outdated vault version by showing appropriate error message.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Order(1)]
+    [Test]
+    public async Task ExtensionOutdatedVaultTest()
+    {
+        // Clear any tracked entities from previous operations.
+        ApiDbContext.ChangeTracker.Clear();
+
+        // Get the latest vault and modify its version to an outdated one.
+        var latestVault = await ApiDbContext.Vaults
+            .OrderByDescending(x => x.RevisionNumber)
+            .FirstAsync();
+
+        latestVault.Version = "1.0.0";
+        await ApiDbContext.SaveChangesAsync();
+
+        // Login to the extension.
+        var extensionPopup = await LoginToExtension(waitForLogin: false);
+
+        // Verify that the expected error message is shown.
+        await extensionPopup.WaitForSelectorAsync("text=Your vault is outdated");
+        var pageContent = await extensionPopup.TextContentAsync("body");
+        Assert.That(pageContent, Does.Contain("Your vault is outdated. Please login via the web client to update your vault."));
+    }
+}
