d35121dbc8e138a0b31566612507bd903e41384c
Leendert de Borst <ldeborst@xivisoft.com>
2025-03-29 16:41:19 +0100

    Add 1Password importer (#542)

--- src/Tests/AliasVault.UnitTests/Utilities/ImportExportTests.cs
+++ src/Tests/AliasVault.UnitTests/Utilities/ImportExportTests.cs
@@ -20,6 +20,7 @@ public class ImportExportTests
     /// <summary>
     /// Test case for importing credentials from CSV and ensuring all values are present.
     /// </summary>
+    /// <returns>Async task.</returns>
     [Test]
     public async Task ImportCredentialsFromCsv()
     {
@@ -79,16 +80,15 @@ public class ImportExportTests
             Assert.That(importedCredential.ServiceUrl, Is.EqualTo(credential.Service.Url));
             Assert.That(importedCredential.Username, Is.EqualTo(credential.Username));
             Assert.That(importedCredential.Notes, Is.EqualTo(credential.Notes));
-            Assert.That(importedCredential.CreatedAt, Is.EqualTo(credential.CreatedAt));
-            Assert.That(importedCredential.UpdatedAt, Is.EqualTo(credential.UpdatedAt));
+            Assert.That(importedCredential.CreatedAt?.Date, Is.EqualTo(credential.CreatedAt.Date));
+            Assert.That(importedCredential.UpdatedAt?.Date, Is.EqualTo(credential.UpdatedAt.Date));
             Assert.That(importedCredential.Alias!.Gender, Is.EqualTo(credential.Alias!.Gender));
             Assert.That(importedCredential.Alias!.FirstName, Is.EqualTo(credential.Alias!.FirstName));
             Assert.That(importedCredential.Alias!.LastName, Is.EqualTo(credential.Alias!.LastName));
             Assert.That(importedCredential.Alias!.NickName, Is.EqualTo(credential.Alias!.NickName));
             Assert.That(importedCredential.Alias!.BirthDate, Is.EqualTo(credential.Alias!.BirthDate));
-            Assert.That(importedCredential.Alias!.Email, Is.EqualTo(credential.Alias!.Email));
-            Assert.That(importedCredential.Alias!.CreatedAt, Is.EqualTo(credential.Alias!.CreatedAt));
-            Assert.That(importedCredential.Alias!.UpdatedAt, Is.EqualTo(credential.Alias!.UpdatedAt));
+            Assert.That(importedCredential.Alias!.CreatedAt?.Date, Is.EqualTo(credential.Alias!.CreatedAt.Date));
+            Assert.That(importedCredential.Alias!.UpdatedAt?.Date, Is.EqualTo(credential.Alias!.UpdatedAt.Date));
             Assert.That(importedCredential.Password, Is.EqualTo(credential.Passwords.First().Value));
         });
     }
@@ -163,6 +163,44 @@ public class ImportExportTests
             Assert.That(sampleCredential.ServiceUrl, Is.EqualTo("https://strongboxsafe.com"));
             Assert.That(sampleCredential.Username, Is.EqualTo("username"));
             Assert.That(sampleCredential.Password, Is.EqualTo("&3V_$z?Aiw-_x+nbYj"));
+        });
+    }
+
+    /// <summary>
+    /// Test case for importing credentials from 1Password CSV and ensuring all values are present.
+    /// </summary>
+    /// <returns>Async task.</returns>
+    [Test]
+    public async Task ImportCredentialsFrom1PasswordCsv()
+    {
+        // Arrange
+        var fileContent = await ResourceReaderUtility.ReadEmbeddedResourceStringAsync("AliasVault.UnitTests.TestData.Exports.1password_8.csv");
+
+        // Act
+        var importedCredentials = await OnePasswordImporter.ImportFromCsvAsync(fileContent);
+
+        // Assert
+        Assert.That(importedCredentials, Has.Count.EqualTo(4));
+
+        // Test specific entries
+        var twoFactorCredential = importedCredentials.First(c => c.Username == "username2fa");
+        Assert.Multiple(() =>
+        {
+            Assert.That(twoFactorCredential.ServiceName, Is.EqualTo("Test record 2 with 2FA"));
+            Assert.That(twoFactorCredential.Username, Is.EqualTo("username2fa"));
+            Assert.That(twoFactorCredential.Password, Is.EqualTo("password2fa"));
+            Assert.That(twoFactorCredential.TwoFactorSecret, Is.EqualTo("otpauth://totp/Strongbox?secret=PLW4SB3PQ7MKVXY2MXF4NEXS6Y&period=30&algorithm=SHA1&digits=6"));
+            Assert.That(twoFactorCredential.Notes, Is.EqualTo("Notes about 2FA record"));
+        });
+
+        var onePasswordAccount = importedCredentials.First(c => c.ServiceName == "1Password Account (dpatel)");
+        Assert.Multiple(() =>
+        {
+            Assert.That(onePasswordAccount.ServiceName, Is.EqualTo("1Password Account (dpatel)"));
+            Assert.That(onePasswordAccount.ServiceUrl, Is.EqualTo("https://my.1password.com"));
+            Assert.That(onePasswordAccount.Username, Is.EqualTo("derekpatel@aliasvault.net"));
+            Assert.That(onePasswordAccount.Password, Is.EqualTo("passwordexample"));
+            Assert.That(onePasswordAccount.Notes, Is.EqualTo("You can use this login to sign in to your account on 1password.com."));
         });
     }
 }
---
+++ src/Utilities/AliasVault.ImportExport/Models/Imports/OnePasswordCsvRecord.cs
@@ -0,0 +1,73 @@
+//-----------------------------------------------------------------------
+// <copyright file="BitwardenCsvRecord.cs" company="lanedirt">
+// Copyright (c) lanedirt. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using AliasVault.ImportExport.Converters;
+using CsvHelper.Configuration.Attributes;
+
+namespace AliasVault.ImportExport.Models.Imports;
+
+/// <summary>
+/// Represents a 1Password CSV record that is being imported from a 1Password CSV export file.
+/// </summary>
+public class OnePasswordCsvRecord
+{
+    /// <summary>
+    /// Gets or sets the title of the item.
+    /// </summary>
+    [Name("Title")]
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the URL of the item.
+    /// </summary>
+    [Name("Url")]
+    public string Url { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the username of the item.
+    /// </summary>
+    [Name("Username")]
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the password of the item.
+    /// </summary>
+    [Name("Password")]
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the OTP (One-Time Password) authentication secret.
+    /// </summary>
+    [Name("OTPAuth")]
+    public string? OTPAuth { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the item is favorited.
+    /// </summary>
+    [Name("Favorite")]
+    [TypeConverter(typeof(BooleanConverter))]
+    public bool Favorite { get; set; }
+
+    /// <summary>
+    /// Gets or sets whether the item is archived.
+    /// </summary>
+    [Name("Archived")]
+    [TypeConverter(typeof(BooleanConverter))]
+    public bool Archived { get; set; }
+
+    /// <summary>
+    /// Gets or sets the service URL.
+    /// </summary>
+    [Name("Tags")]
+    public string? Tags { get; set; }
+
+    /// <summary>
+    /// Gets or sets any additional notes.
+    /// </summary>
+    [Name("Notes")]
+    public string? Notes { get; set; }
+}
