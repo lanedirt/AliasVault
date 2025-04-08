a6c4d1e8a58556233a4f053ea92b976e4722819d
Leendert de Borst <ldeborst@xivisoft.com>
2025-04-08 17:05:24 +0200

    Make credentialmanager work from react native (#771)

---
+++ mobile-app/ios/CredentialManager.m
@@ -0,0 +1,9 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(CredentialManager, NSObject)
+
+RCT_EXTERN_METHOD(addCredential:(NSString *)username password:(NSString *)password service:(NSString *)service)
+RCT_EXTERN_METHOD(clearCredentials)
+RCT_EXTERN_METHOD(requiresMainQueueSetup)
+
+@end 
