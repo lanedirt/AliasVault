ab82a63a0a56cef4a29ce01a14b2c1d2e8b94df7
Leendert de Borst <ldeborst@xivisoft.com>
2025-05-01 08:51:28 +0200

    Bump version to 0.16.2 (#818)

--- src/Shared/AliasVault.Shared.Core/AppInfo.cs
+++ src/Shared/AliasVault.Shared.Core/AppInfo.cs
@@ -30,7 +30,7 @@ public static class AppInfo
     /// <summary>
     /// Gets the patch version number.
     /// </summary>
-    public const int VersionPatch = 1;
+    public const int VersionPatch = 2;
 
     /// <summary>
     /// Gets the minimum supported AliasVault client version. Normally the minimum client version is the same
