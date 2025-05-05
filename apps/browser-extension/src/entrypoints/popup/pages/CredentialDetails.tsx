19a38ba14fbd65a7805095187411a7e0de68196d
Leendert de Borst <ldeborst@xivisoft.com>
2025-05-05 12:40:11 +0200

    Update CredentialDetails.tsx (#771)

--- apps/browser-extension/src/entrypoints/popup/pages/CredentialDetails.tsx
+++ apps/browser-extension/src/entrypoints/popup/pages/CredentialDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useDb } from '../context/DbContext';
 import { Credential } from '../../../utils/types/Credential';
