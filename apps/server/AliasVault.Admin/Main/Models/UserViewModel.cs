96fd9bb837d0ca74833706640bc65f0d76b200e0
Leendert de Borst <ldeborst@xivisoft.com>
2025-05-13 15:14:38 +0200

    Update to AGPLv3 license (#828)

--- apps/server/AliasVault.Admin/Main/Models/UserViewModel.cs
+++ apps/server/AliasVault.Admin/Main/Models/UserViewModel.cs
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------
 // <copyright file="UserViewModel.cs" company="lanedirt">
 // Copyright (c) lanedirt. All rights reserved.
-// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// Licensed under the AGPLv3 license. See LICENSE.md file in the project root for full license information.
 // </copyright>
 //-----------------------------------------------------------------------
 
