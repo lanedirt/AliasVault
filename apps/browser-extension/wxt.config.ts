d99950c24089dba62f0f1368fee9cfdf4fcf26b7
Leendert de Borst <ldeborst@xivisoft.com>
2025-06-16 10:36:37 +0200

    Bump version to 0.19.0 (#926)

--- apps/browser-extension/wxt.config.ts
+++ apps/browser-extension/wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   manifest: {
     name: "AliasVault",
     description: "AliasVault Browser AutoFill Extension. Keeping your personal information private.",
-    version: "0.18.1",
+    version: "0.19.0",
     content_security_policy: {
       extension_pages: "script-src 'self' 'wasm-unsafe-eval'; object-src 'self';"
     },
