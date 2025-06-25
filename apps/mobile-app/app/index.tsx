eaa42196f832088016a5eb4fa8f9f66d021c9b49
Leendert de Borst <ldeborst@xivisoft.com>
2025-06-25 10:46:03 +0200

    Revert app index back to credentials navigation redirect (#957)

--- apps/mobile-app/app/index.tsx
+++ apps/mobile-app/app/index.tsx
@@ -1,9 +1,10 @@
 import { Redirect } from 'expo-router';
 
 /**
- * App index which is the entry point of the app and redirects to the sync screen, which will
- * redirect to the login screen if the user is not logged in or to the main tabs screen if the user is logged in.
+ * App index which is the entry point of the app and redirects to the credentials screen.
+ * If user is not logged in, they will automatically be redirected to the login screen instead
+ * by global navigation handlers.
  */
 export default function AppIndex() : React.ReactNode {
-  return <Redirect href={'/reinitialize'} />
+  return <Redirect href={'/credentials'} />;
 }
