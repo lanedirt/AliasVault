49676bf1f4c458843a377b5cf5ed7d7ea22c401d
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-03 17:57:37 +0200

    Localize passwordstep and credential view page (#820)

---
+++ apps/server/AliasVault.Client/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+//-----------------------------------------------------------------------
+// <copyright file="GlobalSuppressions.cs" company="lanedirt">
+// Copyright (c) lanedirt. All rights reserved.
+// Licensed under the AGPLv3 license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AliasVault.Client
+{
+    using System.Diagnostics.CodeAnalysis;
+}
