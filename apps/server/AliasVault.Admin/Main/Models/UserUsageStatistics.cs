cca687b61fd2adf39ec9ded668b23276f59ce89f
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-23 21:56:22 +0200

    Add user usage statistics to user details page (#1037)

---
+++ apps/server/AliasVault.Admin/Main/Models/UserUsageStatistics.cs
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------------
+// <copyright file="UserUsageStatistics.cs" company="lanedirt">
+// Copyright (c) lanedirt. All rights reserved.
+// Licensed under the AGPLv3 license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace AliasVault.Admin.Main.Models;
+
+/// <summary>
+/// Model representing user-specific usage statistics for both all-time and recent periods.
+/// </summary>
+public class UserUsageStatistics
+{
+    /// <summary>
+    /// Gets or sets the total number of credentials (all-time).
+    /// </summary>
+    public int TotalCredentials { get; set; }
+
+    /// <summary>
+    /// Gets or sets the total number of email claims (all-time).
+    /// </summary>
+    public int TotalEmailClaims { get; set; }
+
+    /// <summary>
+    /// Gets or sets the total number of received emails (all-time).
+    /// </summary>
+    public int TotalReceivedEmails { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of credentials created in the last 72 hours.
+    /// </summary>
+    public int RecentCredentials72h { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of email claims created in the last 72 hours.
+    /// </summary>
+    public int RecentEmailClaims72h { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of emails received in the last 72 hours.
+    /// </summary>
+    public int RecentReceivedEmails72h { get; set; }
+}
