606d0686e21cc50c89722e132562ba25fe64a849
Leendert de Borst <ldeborst@xivisoft.com>
2025-07-26 22:38:09 +0200

    Update IdentityGenerator return type (#1010)

--- shared/identity-generator/src/factories/IdentityGeneratorFactory.ts
+++ shared/identity-generator/src/factories/IdentityGeneratorFactory.ts
@@ -1,13 +1,13 @@
+import { IdentityGenerator } from "src/implementations/base/IdentityGenerator";
 import { IdentityGeneratorEn } from "src/implementations/IdentityGeneratorEn";
 import { IdentityGeneratorNl } from "src/implementations/IdentityGeneratorNl";
-import { IIdentityGenerator } from "src/interfaces/IIdentityGenerator";
 
 /**
  * Creates a new identity generator based on the language.
  * @param language - The language to use for generating the identity (e.g. "en", "nl").
  * @returns A new identity generator instance.
  */
-export const CreateIdentityGenerator = (language: string): IIdentityGenerator => {
+export const CreateIdentityGenerator = (language: string): IdentityGenerator => {
   switch (language) {
     case 'en':
       return new IdentityGeneratorEn();
