68102c38b9b7a5b343d2bab1a70ef5a31a21e5a6
Leendert de Borst <ldeborst@xivisoft.com>
2025-08-28 13:06:56 +0200

    Refactor client form model validation messages and add missing translations (#1153)

--- apps/server/AliasVault.Client/Auth/Models/LoginFormModel.cs
+++ apps/server/AliasVault.Client/Auth/Models/LoginFormModel.cs
@@ -9,36 +9,26 @@ namespace AliasVault.Client.Auth.Models;
 
 using System.ComponentModel.DataAnnotations;
 using AliasVault.Client.Resources;
-using AliasVault.Shared.Models.WebApi.Auth;
 
 /// <summary>
 /// Login form model with localized validation.
 /// </summary>
-public class LoginFormModel : LoginModel
+public class LoginFormModel
 {
     /// <summary>
     /// Gets or sets the username.
     /// </summary>
     [Required(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.UsernameRequired))]
-    public new string Username { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets the password.
     /// </summary>
     [Required(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.PasswordRequired))]
-    public new string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
     /// <summary>
-    /// Converts the form model to the base model.
+    /// Gets or sets a value indicating whether the user wants to be remembered.
     /// </summary>
-    /// <returns>The base LoginModel.</returns>
-    public LoginModel ToBaseModel()
-    {
-        return new LoginModel
-        {
-            Username = Username,
-            Password = Password,
-            RememberMe = RememberMe,
-        };
-    }
+    public bool RememberMe { get; set; } = true;
 }
--- apps/server/AliasVault.Client/Auth/Models/RegisterFormModel.cs
+++ apps/server/AliasVault.Client/Auth/Models/RegisterFormModel.cs
@@ -10,51 +10,35 @@ namespace AliasVault.Client.Auth.Models;
 using System.ComponentModel.DataAnnotations;
 using AliasVault.Client.Resources;
 using AliasVault.Shared.Models.Validation;
-using AliasVault.Shared.Models.WebApi.Auth;
 
 /// <summary>
 /// Register form model with validation.
 /// </summary>
-public class RegisterFormModel : RegisterModel
+public class RegisterFormModel
 {
     /// <summary>
     /// Gets or sets the username.
     /// </summary>
     [Required(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.UsernameRequired))]
-    public new string Username { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets the password.
     /// </summary>
     [Required(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.PasswordRequired))]
     [MinLength(8, ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.PasswordMinLengthGeneric))]
-    public new string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets the password confirmation.
     /// </summary>
     [Required(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.PasswordRequired))]
     [Compare("Password", ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.PasswordsDoNotMatchGeneric))]
-    public new string PasswordConfirm { get; set; } = null!;
+    public string PasswordConfirm { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets a value indicating whether the terms and conditions are accepted or not.
     /// </summary>
     [MustBeTrue(ErrorMessageResourceType = typeof(ValidationMessages), ErrorMessageResourceName = nameof(ValidationMessages.MustAcceptTerms))]
-    public new bool AcceptTerms { get; set; } = false;
-
-    /// <summary>
-    /// Converts the form model to the base model.
-    /// </summary>
-    /// <returns>The base RegisterModel.</returns>
-    public RegisterModel ToBaseModel()
-    {
-        return new RegisterModel
-        {
-            Username = Username,
-            Password = Password,
-            PasswordConfirm = PasswordConfirm,
-            AcceptTerms = AcceptTerms,
-        };
-    }
+    public bool AcceptTerms { get; set; } = false;
 }
