ce20211092996f2fd74170ffd28a15c840b8e418
Leendert de Borst <ldeborst@xivisoft.com>
2025-08-29 16:43:05 +0200

    Add stubs for new NativeVaultManager spec for iOS (#1157)

--- apps/mobile-app/ios/NativeVaultManager/RCTNativeVaultManager.mm
+++ apps/mobile-app/ios/NativeVaultManager/RCTNativeVaultManager.mm
@@ -141,4 +141,26 @@
     [vaultManager copyToClipboardWithExpiration:text expirationSeconds:expirationSeconds resolver:resolve rejecter:reject];
 }
 
+// MARK: - Android-specific methods (stubs for iOS)
+
+- (void)canScheduleExactAlarms:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    // Only used by Android, return true.
+    resolve(@(YES));
+}
+
+- (void)requestExactAlarmPermission:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    // Only used by Android, return true.
+    resolve(@"Not applicable on iOS");
+}
+
+- (void)isIgnoringBatteryOptimizations:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    // Only used by Android, return true.
+    resolve(@(YES));
+}
+
+- (void)requestIgnoreBatteryOptimizations:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
+    // Only used by Android, return true.
+    resolve(@"Not applicable on iOS");
+}
+
 @end
