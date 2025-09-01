c24671ffb1feffe9777d61131ed10d8e48533cbe
Leendert de Borst <ldeborst@xivisoft.com>
2025-09-01 16:56:53 +0200

    Bump version to 0.22.0 (#1163)

--- apps/browser-extension/src/utils/AppInfo.ts
+++ apps/browser-extension/src/utils/AppInfo.ts
@@ -6,7 +6,7 @@ export class AppInfo {
   /**
    * The current extension version. This should be updated with each release of the extension.
    */
-  public static readonly VERSION = '0.21.2';
+  public static readonly VERSION = '0.22.0';
 
   /**
    * The minimum supported AliasVault server (API) version. If the server version is below this, the
--- apps/mobile-app/utils/AppInfo.ts
+++ apps/mobile-app/utils/AppInfo.ts
@@ -8,7 +8,7 @@ export class AppInfo {
   /**
    * The current mobile app version. This should be updated with each release of the mobile app.
    */
-  public static readonly VERSION = '0.21.2';
+  public static readonly VERSION = '0.22.0';
 
   /**
    * The minimum supported AliasVault server (API) version. If the server version is below this, the
